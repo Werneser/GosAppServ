1f65069370c482732aaac173189132aa17c26be4
Werneser <mishaaldeny@gmail.com>
2025-12-20 13:54:27 +0300

    мод

--- Gosuslugi/src/main/kotlin/gosuslugi/DTOs/UserInformationDTO.kt
+++ Gosuslugi/src/main/kotlin/gosuslugi/DTOs/UserInformationDTO.kt
@@ -3,14 +3,6 @@ package gosuslugi.DTOs
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class UserCredentialsDTO(
-    val userId: String,
-    val login: String,
-    val password: String,
-    val token: String?
-)
-
-@Serializable
 data class UserInformationDTO(
     val userId: String,
     val userPhone: String,
